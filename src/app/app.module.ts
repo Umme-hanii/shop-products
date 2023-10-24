27c2a443c2d2a0fe79d5f36c1bf6ff095f973f22
Umme-hanii <ummehanimohammed9@gmail.com>
2023-10-24 12:46:47 +0530

    Design Toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { MaterialDesignModule } from './modules/material-design.module'
+import { NavBarComponent } from './components/nav-bar/nav-bar.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, NavBarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialDesignModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/material-design.module.ts
+++ src/app/modules/material-design.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
-  imports: [],
-  exports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  exports: [MatToolbarModule, MatIconModule, MatButtonModule],
 })
 export class MaterialDesignModule {}
